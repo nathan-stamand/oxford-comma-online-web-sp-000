1bbeba8a80d12c4e22125a78d232f739f96a303a
Nathan St.Amand <nathansaint810@gmail.com>
2020-02-18 12:52:31 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
-
-end
+  case array.length
+    when 1
+      return array[0]
+    when 2 
+      return array.join(" and ")
+    else
+      i = 0;
+      new_array = []
+      while i < array.length-1 do
+        new_array.push(array[i])
+        i += 1
+      end
+      "#{new_array.join(", ")}, and #{array[array.length-1]}"
+    end
+  end
